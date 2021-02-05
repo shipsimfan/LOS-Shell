0eb773be32f5351add783a3cd2d28f344bf2303c
Lance Hart <shipsimfan@hotmail.com>
2021-02-04 23:30:59 -0500

    Added keyboard input

--- src/crt0.asm
+++ src/crt0.asm
@@ -7,14 +7,21 @@ _start:
 
     call main
 
-    mov rdi, 0
-    syscall
+    ;mov rdi, 0
+    ;syscall
 
     mov rdi, 1
     mov rsi, message
+    syscall
+
+    mov rdi, 2
+    mov rsi, buffer
+    mov rdx, 2048
     syscall
 
     .hang:
         jmp .hang
 
 message: db "Hello World!", 0x0A, 0x00
+
+buffer: resb 2048
