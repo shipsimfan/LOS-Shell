0a01f1095718d8dde5e70564633f3b743d1bcabf
Lance Hart <shipsimfan@hotmail.com>
2021-03-07 22:30:15 -0500

    Updated to previous functionality

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@
 char buffer[128];
 
 int main() {
-    DisplayString("LOS Shell v0.1\n");
+    DisplayString("\n\nLOS Shell v0.1\n\n");
 
-    /*while (1) {
+    while (1) {
         DisplayString("\n> ");
         ReadLine(buffer, 128);
         DisplayString("\n");
@@ -23,7 +23,7 @@ int main() {
         } else {
             DisplayString(buffer);
         }
-    }*/
+    }
 
     return 0;
 }
