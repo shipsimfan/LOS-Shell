8862a8fc77a6e7643788f6448da5a9fe10603c23
Lance Hart <shipsimfan@hotmail.com>
2021-03-10 17:27:02 -0500

    Updated user input method

--- src/main.c
+++ src/main.c
@@ -3,25 +3,78 @@
 #include <string.h>
 #include <unistd.h>
 
-char buffer[128];
+char* getline() {
+    char* line = malloc(128);
+    char* ptr = line;
+    size_t maxLen = 128;
+    size_t len = 0;
+    int c;
+
+    if (line == NULL)
+        return NULL;
+
+    while (1) {
+        c = fgetc(stdin);
+        if (c == 0)
+            break;
+
+        len++;
+        if (len == maxLen) {
+            maxLen <<= 1;
+            char* newLine = malloc(maxLen);
+            if (newLine == NULL) {
+                free(line);
+                return NULL;
+            }
+
+            memcpy(newLine, line, len);
+
+            free(line);
+            line = newLine;
+
+            ptr = line + len;
+        }
+
+        if (c == '\b') {
+            len--;
+            if (len > 0) {
+                putchar(c);
+                ptr--;
+            }
+        } else {
+            putchar(c);
+
+            *ptr = c;
+            ptr++;
+            if (c == '\n')
+                break;
+        }
+    }
+
+    *ptr = 0;
+    return line;
+}
 
 int main() {
     printf("\nLOS Shell v0.2");
 
+    char* line;
     while (1) {
         printf("\n> ");
-        fread(buffer, sizeof(char), 128, stdin);
+        line = getline();
         printf("\n");
 
-        if (strcmp(buffer, "EXIT") == 0)
+        if (line == NULL)
+            abort();
+
+        if (strcmp(line, "exit") == 0)
             break;
-        else if (buffer[0] == ';') {
-            buffer[0] = ':';
-            pid_t pid = execute(buffer);
+        else if (line[0] == ':') {
+            pid_t pid = execute(line);
             int status = wait(pid);
             printf("Child exited with status %i\n", status);
         } else
-            printf("%s", buffer);
+            printf("%s", line);
     }
 
     return 0;
