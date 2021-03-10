0de584ff79431bc6c1db6131a56cdae831589a92
Lance Hart <shipsimfan@hotmail.com>
2021-03-10 13:09:17 -0500

    Added wait and execute

--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 char buffer[128];
 
 int main() {
-
     printf("\nLOS Shell v0.2\n\n");
 
     while (1) {
@@ -15,7 +15,12 @@ int main() {
 
         if (strcmp(buffer, "EXIT") == 0)
             break;
-        else
+        else if (buffer[0] == ';') {
+            buffer[0] = ':';
+            pid_t pid = execute(buffer);
+            int status = wait(pid);
+            printf("Child exited with status %i\n", status);
+        } else
             printf("%s\n", buffer);
     }
 
