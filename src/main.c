cf0a68080140ddef0ec9ff2a697366eb7a451eeb
Lance Hart <shipsimfan@hotmail.com>
2021-03-10 10:52:37 -0500

    Improved printing

--- src/main.c
+++ src/main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 char buffer[128];
 
 int main() {
-    printf("\nLOS Shell v0.2\n");
+
+    printf("\nLOS Shell v0.2\n\n");
 
     while (1) {
-        printf("\n> ");
+        printf("> ");
         fread(buffer, sizeof(char), 128, stdin);
+        printf("\n");
 
         if (strcmp(buffer, "EXIT") == 0)
             break;
         else
-            printf("\n%s", buffer);
+            printf("%s\n", buffer);
     }
 
     return 0;
